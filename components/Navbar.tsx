9ee991b01c6e7848f494ec4ec882d3ae7817c3b3
Kennedy Omondi <kencarsonbonyo@gmail.com>
2024-05-22 02:54:11 +0300

    Banners added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,14 +5,13 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav className='flex-center fixed top-0 z-50 w-full border-b-2 border-r-black-200 bg-black-100 py-7 text-white'>
-        <div className='flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-6'>
-            <Link href="/">
-                <Image src="/jsm-logo.svg"
-                    alt="logo"
-                    width={55}
-                    height={40}
-                />
-            </Link>
+        <div className='flex-between mx-auto w-full max-w-screen-3xl px-6 xs:px-8 sm:px-6'>
+        <ul className='flex-center gap-x-3 max-md:hidden md:gap-x-10'>
+                <li className='body-text text-gradient_blue-purple !font-bold'>
+                    <Link href="https://colken.co.ke" target='_blank'> Nashon Angudha </Link>
+                </li>
+
+            </ul>
             <Image
             src="/hamburger-menu.svg"
             width={30}
@@ -21,11 +20,9 @@ const Navbar = () => {
             className='block md:hidden'
             />
             <ul className='flex-center gap-x-3 max-md:hidden md:gap-x-10'>
-                <li className='body-text text-gradient_blue-purple !font-bold'>
-                    <Link href="https://colken.co.ke" target='_blank'> Next.Js 13.4</Link>
-                </li>
+
                 <li className='body-text !font-normal'>
-                    <Link href="https://colken.co.ke/harambee" target='_blank'> Masterclass</Link>
+                    <Link href="https://colken.co.ke/harambee" target='_blank'> Book Appointment</Link>
                 </li>
 
             </ul>
